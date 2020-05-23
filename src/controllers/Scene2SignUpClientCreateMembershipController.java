ed63c6f182f2469a944ce94e527fea65c61c8d8a
Guiman Daniel-Marius <daniel.mariusgdm@gmail.com>
2020-05-23 22:02:13 +0300

    1st Scene create membership works now

--- src/controllers/Scene2SignUpClientCreateMembershipController.java
+++ src/controllers/Scene2SignUpClientCreateMembershipController.java
@@ -11,11 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 public class Scene2SignUpClientCreateMembershipController {
@@ -34,8 +36,9 @@ public class Scene2SignUpClientCreateMembershipController {
 
     private Connection connection;
     private DBHandler handler;
-    private ResultSet rs1, rs2, rs3;
-    private PreparedStatement ps1, ps2, ps3, ps4;
+    private ResultSet rs1, rs2, rs3, rs3a, rs4;
+    private PreparedStatement ps1, ps2, ps3, ps3a, ps4;
+    private String dC;
 
     public void initialize(){
         buttonNextPageMembership.setDisable(true);
@@ -43,7 +46,7 @@ public class Scene2SignUpClientCreateMembershipController {
         connection = handler.getConnection();
         String sql1 = "SELECT * FROM gyms";
         String sql2 = "SELECT * FROM diets";
-        String sql3 = "SELECT * FROM diettype";
+
         try{
             ps1 = connection.prepareStatement(sql1);
             rs1 = ps1.executeQuery();
@@ -53,22 +56,15 @@ public class Scene2SignUpClientCreateMembershipController {
             ps2 = connection.prepareStatement(sql2);
             rs2 = ps2.executeQuery();
             while(rs2.next()){
-                comboBoxSelectDiet.getItems().addAll(rs2.getString("dietType")); //vegan-nonvegan
-            }
-            ps3 = connection.prepareStatement(sql3);
-            rs3 = ps3.executeQuery();
-            while(rs3.next()){
-                comboBoxSelectDietBreakfast.getItems().addAll(rs3.getString("breakfast") + " " +
-                        "(" + rs3.getInt("breakfastCal") + " Cal.)");
-                comboBoxSelectDietLunch.getItems().addAll(rs3.getString("Lunch") + " " +
-                        "(" + rs3.getInt("LunchCal") + " Cal.)");
-                comboBoxSelectDietDinner.getItems().addAll(rs3.getString("Dinner") + " " +
-                        "(" + rs3.getInt("DinnerCal") + " Cal.)");
+                comboBoxSelectDiet.getItems().addAll(rs2.getString("dietType"));
             }
         }catch (Exception e){
             e.getCause();
             e.printStackTrace();
         }
+        comboBoxSelectDietBreakfast.setDisable(true);
+        comboBoxSelectDietDinner.setDisable(true);
+        comboBoxSelectDietLunch.setDisable(true);
     }
 
     public String getGymChoice(){
@@ -83,14 +79,65 @@ public class Scene2SignUpClientCreateMembershipController {
        // return (String) comboBoxSelectTypeOfDiet.getSelectionModel().getSelectedItem();
    // }
 
-    public void keyReleasedProperty(){
+    public void keyReleasedProperty() throws SQLException {
         boolean bln = (comboBoxSelectGym.getSelectionModel().isEmpty() || comboBoxSelectDiet.getSelectionModel().isEmpty() ||
                         comboBoxSelectDietBreakfast.getSelectionModel().isEmpty() || comboBoxSelectDietLunch.getSelectionModel().isEmpty()
                         || comboBoxSelectDietDinner.getSelectionModel().isEmpty());
-;
+        dC = getDietChoice();
+        String sql3 = "SELECT * FROM diettypenv"; // normal diet(nonvegan)
+        String sql3a = "SELECT * FROM diettypev"; //vegans
+        try {
+            if (dC.equals("Normal diet")) {
+                if (comboBoxSelectDietBreakfast.getSelectionModel().isEmpty())
+                    comboBoxSelectDietBreakfast.getItems().clear();
+                if (comboBoxSelectDietLunch.getSelectionModel().isEmpty())
+                    comboBoxSelectDietLunch.getItems().clear();
+                if (comboBoxSelectDietDinner.getSelectionModel().isEmpty())
+                    comboBoxSelectDietDinner.getItems().clear();
+                ps3 = connection.prepareStatement(sql3);
+                rs3 = ps3.executeQuery();
+                while (rs3.next()) {
+                    comboBoxSelectDietBreakfast.getItems().addAll(rs3.getString("breakfast") + " " +
+                            "(" + rs3.getInt("breakfastCal") + " Cal.)");
+                    comboBoxSelectDietLunch.getItems().addAll(rs3.getString("Lunch") + " " +
+                            "(" + rs3.getInt("LunchCal") + " Cal.)");
+                    comboBoxSelectDietDinner.getItems().addAll(rs3.getString("Dinner") + " " +
+                            "(" + rs3.getInt("DinnerCal") + " Cal.)");
+                }
+            } else {
+                if (comboBoxSelectDietBreakfast.getSelectionModel().isEmpty())
+                    comboBoxSelectDietBreakfast.getItems().clear();
+                if (comboBoxSelectDietLunch.getSelectionModel().isEmpty())
+                    comboBoxSelectDietLunch.getItems().clear();
+                if (comboBoxSelectDietDinner.getSelectionModel().isEmpty())
+                    comboBoxSelectDietDinner.getItems().clear();
+                ps3a = connection.prepareStatement(sql3a);
+                rs3a = ps3a.executeQuery();
+                while (rs3a.next()) {
+                    comboBoxSelectDietBreakfast.getItems().addAll(rs3a.getString("breakfast") + " " +
+                            "(" + rs3a.getInt("breakfastCal") + " Cal.)");
+                    comboBoxSelectDietLunch.getItems().addAll(rs3a.getString("Lunch") + " " +
+                            "(" + rs3a.getInt("LunchCal") + " Cal.)");
+                    comboBoxSelectDietDinner.getItems().addAll(rs3a.getString("Dinner") + " " +
+                            "(" + rs3a.getInt("DinnerCal") + " Cal.)");
+                }
+            }
+        }catch (Exception e){
+            System.out.println("Clientul nu a selectat inca tipul de dieta");
+        }
+        boolean bln1 = (comboBoxSelectDiet.getSelectionModel().isEmpty());
+        comboBoxSelectDietBreakfast.setDisable(bln1);
+        comboBoxSelectDietLunch.setDisable(bln1);
+        comboBoxSelectDietDinner.setDisable(bln1);
+
         buttonNextPageMembership.setDisable(bln);
     }
-
+    /* comboBoxSelectDietBreakfast.getItems().addAll(rss.getString("breakfast") + " " +
+                                    "(" + rss.getInt("breakfastCal") + " Cal.)");
+                            comboBoxSelectDietLunch.getItems().addAll(rss.getString("Lunch") + " " +
+                                    "(" + rss.getInt("LunchCal") + " Cal.)");
+                            comboBoxSelectDietDinner.getItems().addAll(rss.getString("Dinner") + " " +
+                                    "(" + rss.getInt("DinnerCal") + " Cal.)");*/
     public void onClickGoToNextPage() throws IOException {
         /*String sql4 = "INSERT INTO membership(gymId) VALUES(?)";
         ps4  = connection.prepareStatement(sql4);
@@ -106,7 +153,7 @@ public class Scene2SignUpClientCreateMembershipController {
         ps4.executeUpdate();
         */
         buttonNextPageMembership.getScene().getWindow().hide();
-        Parent root = FXMLLoader.load(getClass().getResource("/resources/view/Scene2aSignUpClientCreatMembership.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/view/Scene2aSignUpClientCreateMembership.fxml"));
         Stage signUpClient2 = new Stage();
         signUpClient2.setScene(new Scene(root));
         signUpClient2.setTitle("RAW POWER GYM - Create your MEMBERSHIP");
